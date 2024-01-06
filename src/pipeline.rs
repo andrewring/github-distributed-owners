6e0bfb54661f188ab623f3d55803a858d1e6b366
Andrew Ring <andrew@andrewring.dev>
2024-01-06 12:56:38 -0600

    Add ability to insert a custom message in auto generated notice

--- src/pipeline.rs
+++ src/pipeline.rs
@@ -5,33 +5,53 @@ use indoc::indoc;
 use std::fs;
 use std::fs::create_dir_all;
 use std::path::PathBuf;
+use textwrap::wrap;
 
-const AUTO_GENERATED_NOTICE: &str = indoc! {"\
-    ################################################################################
-    #                             AUTO GENERATED FILE
-    #                            Do Not Manually Update
-    #                              For details, see:
-    #        https://github.com/andrewring/github-distributed-owners#readme
-    ################################################################################"
-};
+fn get_auto_generated_notice<S: AsRef<str>>(message: Option<S>) -> String {
+    let mut out = indoc! {"\
+        ################################################################################
+        #                             AUTO GENERATED FILE
+        #                            Do Not Manually Update
+        "
+    }
+    .to_string();
 
-pub fn generate_codeowners_from_files<F>(
+    if let Some(message) = message {
+        wrap(message.as_ref(), 78).iter().for_each(|line| {
+            out.push_str(format!("# {: ^78}", line).trim());
+            out.push('\n');
+        });
+    }
+
+    out.push_str(indoc! {"\
+        #                              For details, see:
+        #        https://github.com/andrewring/github-distributed-owners#readme
+        ################################################################################"
+    });
+
+    out
+}
+
+pub fn generate_codeowners_from_files<F, S>(
     repo_root: Option<PathBuf>,
     output_file: Option<PathBuf>,
     implicit_inherit: bool,
     allow_filter: &F,
+    message: Option<S>,
 ) -> anyhow::Result<()>
 where
     F: AllowFilter,
+    S: AsRef<str>,
 {
     let root = repo_root.unwrap_or(std::env::current_dir()?);
     let tree = OwnersTree::load_from_files(root, allow_filter)?;
 
     let codeowners = generate_codeowners(&tree, implicit_inherit)?;
     let mut codeowners_text = to_codeowners_string(codeowners);
+    let auto_generated_notice = get_auto_generated_notice(message);
 
     codeowners_text =
-        format!("{AUTO_GENERATED_NOTICE}\n\n{codeowners_text}\n\n{AUTO_GENERATED_NOTICE}");
+        format!("{auto_generated_notice}\n\n{codeowners_text}\n\n{auto_generated_notice}");
 
     match output_file {
         None => println!("{}", codeowners_text),
@@ -53,7 +73,7 @@ where
 #[cfg(test)]
 mod test {
     use crate::allow_filter::FilterGitMetadata;
-    use crate::pipeline::generate_codeowners_from_files;
+    use crate::pipeline::{generate_codeowners_from_files, get_auto_generated_notice};
     use crate::test_utils::create_test_file;
     use indoc::indoc;
     use std::fs;
@@ -62,7 +82,7 @@ mod test {
     const ALLOW_ANY: FilterGitMetadata = FilterGitMetadata {};
 
     #[test]
-    fn test_simple() -> anyhow::Result<()> {
+    fn test_generate_codeowners_from_files_simple() -> anyhow::Result<()> {
         let temp_dir = tempdir()?;
         let root_dir = temp_dir.path();
         create_test_file(
@@ -100,12 +120,14 @@ mod test {
         let output_file = root_dir.join("CODEOWNERS");
         let repo_root = Some(root_dir.to_path_buf());
         let implicit_inherit = true;
+        let message = Option::<String>::None;
 
         generate_codeowners_from_files(
             repo_root,
             Some(output_file.clone()),
             implicit_inherit,
             &ALLOW_ANY,
+            message,
         )?;
 
         let generated_codeowners = fs::read_to_string(output_file)?;
@@ -116,7 +138,7 @@ mod test {
     }
 
     #[test]
-    fn test_multiple_files() -> anyhow::Result<()> {
+    fn test_generate_codeowners_from_files_multiple_files() -> anyhow::Result<()> {
         let temp_dir = tempdir()?;
         let root_dir = temp_dir.path();
         create_test_file(
@@ -161,12 +183,14 @@ mod test {
         let output_file = root_dir.join("CODEOWNERS");
         let repo_root = Some(root_dir.to_path_buf());
         let implicit_inherit = true;
+        let message = Option::<String>::None;
 
         generate_codeowners_from_files(
             repo_root,
             Some(output_file.clone()),
             implicit_inherit,
             &ALLOW_ANY,
+            message,
         )?;
 
         let generated_codeowners = fs::read_to_string(output_file)?;
@@ -177,7 +201,7 @@ mod test {
     }
 
     #[test]
-    fn test_multiple_files_with_overrides() -> anyhow::Result<()> {
+    fn test_generate_codeowners_from_files_multiple_files_with_overrides() -> anyhow::Result<()> {
         let temp_dir = tempdir()?;
         let root_dir = temp_dir.path();
         create_test_file(
@@ -227,12 +251,14 @@ mod test {
         let output_file = root_dir.join("CODEOWNERS");
         let repo_root = Some(root_dir.to_path_buf());
         let implicit_inherit = true;
+        let message = Option::<String>::None;
 
         generate_codeowners_from_files(
             repo_root,
             Some(output_file.clone()),
             implicit_inherit,
             &ALLOW_ANY,
+            message,
         )?;
 
         let generated_codeowners = fs::read_to_string(output_file)?;
@@ -243,7 +269,7 @@ mod test {
     }
 
     #[test]
-    fn test_empty_root_blanket_owners() -> anyhow::Result<()> {
+    fn test_generate_codeowners_from_files_empty_root_blanket_owners() -> anyhow::Result<()> {
         let temp_dir = tempdir()?;
         let root_dir = temp_dir.path();
         create_test_file(
@@ -294,12 +320,14 @@ mod test {
         let output_file = root_dir.join("CODEOWNERS");
         let repo_root = Some(root_dir.to_path_buf());
         let implicit_inherit = true;
+        let message = Option::<String>::None;
 
         generate_codeowners_from_files(
             repo_root,
             Some(output_file.clone()),
             implicit_inherit,
             &ALLOW_ANY,
+            message,
         )?;
 
         let generated_codeowners = fs::read_to_string(output_file)?;
@@ -307,5 +335,51 @@ mod test {
         assert_eq!(generated_codeowners, expected);
 
         Ok(())
+    }
+
+    #[test]
+    fn test_get_auto_generated_notice_default() {
+        let expected = indoc! {"\
+            ################################################################################
+            #                             AUTO GENERATED FILE
+            #                            Do Not Manually Update
+            #                              For details, see:
+            #        https://github.com/andrewring/github-distributed-owners#readme
+            ################################################################################"
+        };
+        assert_eq!(get_auto_generated_notice::<String>(None), expected);
+    }
+
+    #[test]
+    fn test_get_auto_generated_notice_short() {
+        let expected = indoc! {"\
+            ################################################################################
+            #                             AUTO GENERATED FILE
+            #                            Do Not Manually Update
+            #                          Some short text on one line
+            #                              For details, see:
+            #        https://github.com/andrewring/github-distributed-owners#readme
+            ################################################################################"
+        };
+        let message = "Some short text on one line";
+        assert_eq!(get_auto_generated_notice(Some(message)), expected);
+    }
+
+    #[test]
+    fn test_get_auto_generated_notice_multiline() {
+        let expected = indoc! {"\
+            ################################################################################
+            #                             AUTO GENERATED FILE
+            #                            Do Not Manually Update
+            # A much longer custom message which doesn't fit on a single line. It will need
+            #                   to be wrapped into multiple lines, neatly.
+            #                              For details, see:
+            #        https://github.com/andrewring/github-distributed-owners#readme
+            ################################################################################"
+        };
+        let message =
+            "A much longer custom message which doesn't fit on a single line. It will need to be wrapped into multiple \
+            lines, neatly.";
+        assert_eq!(get_auto_generated_notice(Some(message)), expected);
     }
 }
